6f296f6faaa1a8797b28c6bbda305b57cc40a420
liangdiyuan <1104152218@qq.com>
2022-04-11 17:20:28 +0800

    添加全量编译

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
-// const babelPluginTransformVueTemplate = require("./lib/index");
+const babelPluginTransformVueTemplate = require("./lib/index");
 
 module.exports = function (api) {
   api.cache(true);
   return {
-    plugins: [['babel-plugin-transform-vue-inline-template-next']],
+    plugins: [[babelPluginTransformVueTemplate]],
   };
 };
---
+++ build/compileAll.test.js
@@ -0,0 +1,41 @@
+new Vue({
+  el: "#app",
+  data: function () {
+    return {
+      age: 18
+    };
+  },
+
+  /* custom ignore1 */
+  template: `
+    <div @click="testFn" :test="test2">{{test}}</div>
+  `,
+  tes: {}
+});
+var a = {
+  /* ignore */
+  template: `
+      <div @click="testFn" :test="test2">{{test}}</div>
+    `
+};
+var c = {
+  /* custom ignore1 */
+  template: `
+    <div @click="testFn" :test="test2">{{test}}</div>
+  `
+}; // ignore
+
+var e = {
+  template: `
+    <div @click="testFn" :test="test2">{{test}}</div>
+  `
+};
+var d = {
+  template: ``
+};
+var template = `
+<div @click="testFn" :test="test2">{{test}}</div>
+`;
+var b = {
+  template: template
+};
